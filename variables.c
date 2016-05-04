55f41d9b912bc9879d8876f5ce02269a50f6d143
rain1 <rain1@openmailbox.org>
2016-05-04 14:06:52 +0100

    basic variable name support

--- variables.c
+++ variables.c
@@ -1,14 +1,73 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
+#include "tokenizer.h"
 #include "variables.h"
+
+char variable_name[TOK_MAX];
 
 char *expand_variables(char *tok, int t, char **envp)
 {
-	char *o;
+	char *stok, *o, *val;
+	int alloc_len;
+	int i, l;
 
-	o = malloc(t+1);
-	memcpy(o, tok, t+1);
+	stok = tok;
+	alloc_len = t+1;
+	o = malloc(alloc_len);
+
+	i = 0;
+	// TODO bounds
+	while (*tok) {
+		if (*tok == '$') {
+			if (!(tok = read_variable_prefix(tok))) {
+				fprintf(stderr, "Error parsing variable inside token [%s] at character [%d]", stok, i);
+				exit(-1);
+			}
+
+			if (!(val = getenv(variable_name))) {
+				fprintf(stderr, "Error parsing variable inside token [%s] at character [%d]", stok, i);
+				exit(-1);
+			}
+
+			l = strlen(val);
+			alloc_len += l;
+			realloc(o, alloc_len);
+			memcpy(o + i, val, l);
+			i += l;
+		}
+		else {
+			o[i++] = *tok++;
+		}
+	}
+	o[i] = '\0';
 
 	return o;
+}
+
+int variable_character(char c)
+{
+	return c == '_' || ('A' <= c && c <= 'Z') || ('0' <= c && c <= '9');
+}
+
+char *read_variable_prefix(char *tok)
+{
+	// TODO: verify that the first letter is not a digit?
+	int i;
+
+	assert(*tok == '$');
+	tok++;
+
+	i = 0;
+	// TODO: bounds checking
+	while (variable_character(*tok))
+		variable_name[i++] = *tok++;
+	variable_name[i] = '\0';
+
+	if (!i)
+		return NULL;
+	
+	return tok;
 }
--- variables.h
+++ variables.h
@@ -1,2 +1,6 @@
+extern char variable_name[TOK_MAX];
+
+char *read_variable_prefix(char *tok);
+int variable_character(char c);
 char *expand_variables(char *tok, int t, char **envp);
 
