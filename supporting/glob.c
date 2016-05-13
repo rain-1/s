7e7250861a01189b7c86cd1946ff728298dc8b07
rain1 <rain1@openmailbox.org>
2016-05-13 04:21:21 +0100

    bugfix in glob

--- supporting/glob.c
+++ supporting/glob.c
@@ -7,12 +7,16 @@ int main(int argc, char **argv) {
 	int i;
 	glob_t g;
 
+	argc--;
+	argv++;
+
 	g.gl_offs = 0;
+
 	for (i = 0; i < argc; i++) {
 		glob(argv[i], GLOB_DOOFFS | (i ? GLOB_APPEND : 0) | (strchr(argv[i], '*') ? 0 : GLOB_NOCHECK), NULL, &g);
 	}
 
-	execvp(g.gl_pathv[0], g.gl_pathv+1);
+	execvp(g.gl_pathv[0], g.gl_pathv);
 
 	fprintf(stderr, "Error: could not execute %s\n", g.gl_pathv[0]);
 	return 1;
