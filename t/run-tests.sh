207a0076b64e880a9377834ec4a02ba052f0f1df
rain <rain1@openmailbox.org>
2017-06-23 13:24:05 +0000

    divide the tests into test/ and expect/ folders

--- t/run-tests.sh
+++ t/run-tests.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-for FILE in *.test
+for FILE in test/*.test
 do
 	EXPECT=${FILE%.test}.expect
+	EXPECT=expect${EXPECT#test}
 	
 	echo "running $FILE expecting $EXPECT."
 	../s "$FILE" 2>&1 | diff -q "$EXPECT" - || (echo "fail" && exit)
