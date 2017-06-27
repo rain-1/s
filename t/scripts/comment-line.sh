98c1ea258591a9c517d2fba6a2dbe199b83a0084
rain <rain1@openmailbox.org>
2017-06-27 14:09:17 +0000

    add tests for comments

---
+++ t/scripts/comment-line.sh
@@ -0,0 +1,1 @@
+echo here goes # nothing
---
+++ t/scripts/comment-multi.sh
@@ -0,0 +1,9 @@
+echo this is a very complicated script
+# which demands
+# lots and lots
+# of explanation
+# in comments to
+# explain what on
+# earth the next
+# line does
+echo .
